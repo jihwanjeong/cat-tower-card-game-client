04e77ca97e67769dd2e081347cc96c4b95c0fc91
SeungYeon Kim <47077179+seunggom@users.noreply.github.com>
2021-01-10 13:14:13 +0900

    유저 정보 생성

---
+++ CatTower/Assets/Scripts/Message.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CatTower
+{
+    public class Message
+    {
+        private Action m_Listeners;
+
+        public void AddListener(Action listener)
+        {
+            m_Listeners += listener;
+        }
+
+        public void RemoveListener(Action listener)
+        {
+            m_Listeners -= listener;
+        }
+
+        public void Send()
+        {
+            if (m_Listeners != null)
+                m_Listeners();
+        }
+    }
+}

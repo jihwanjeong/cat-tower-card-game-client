002e8f50f8db715f9fe95ee8aa14a02e231132ff
jihwanjeong <53459838+jihwanjeong@users.noreply.github.com>
2021-01-31 20:03:50 +0900

    지환 01/31
    ui작업, 스크립트는 작성중

---
+++ CatTower/Assets/playerinfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CatTower
+{
+    public class NewBehaviourScript : MonoBehaviour
+    {
+        public TEXT userId;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ CatTower/Assets/room_name.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CatTower
+{
+    public class room_name : MonoBehaviour
+    {
+        public Text roomName;
+        // Start is called before the first frame update
+        void Start()
+        {
+            roomName.text = roomId;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
